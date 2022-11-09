220907e405fc07ee808581a50428b6d1c9cd174f
alekspanteli <alekspanteli@rambler.ru>
2022-11-09 22:27:26 +0000

    add layout and pages

---
+++ app/blog/page.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function Blog() {
+    return (
+        <>
+            <h1>Blog page goes here</h1>
+            <Link href="/">Back</Link>
+        </>
+    )
+}
---
+++ app/layout.tsx
@@ -0,0 +1,19 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { Inter } from '@next/font/google'
+
+const inter = Inter()
+
+export default function RootLayout({
+    // Layouts must accept a children prop.
+    // This will be populated with nested layouts or pages
+    children,
+  }: {
+    children: React.ReactNode;
+  }) {
+    return (
+      <html lang="en" className={inter.className}>
+        <body>{children}</body>
+      </html>
+    );
+  }
