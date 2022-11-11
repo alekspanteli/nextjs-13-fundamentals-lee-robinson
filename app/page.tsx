e75d8345803f0328d412118761741d94bc0e8c5c
alekspanteli <alekspanteli@rambler.ru>
2022-11-11 13:10:28 +0000

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,40 @@
 
-import Link from 'next/link';
+// import Link from 'next/link';
 
-export default function Home() {
+import { Key } from "react";
+
+// export default function Home() {
+//   return (
+//     <div>
+//       <h1>Home page</h1>
+//       <Link href="/blog">Blog</Link>
+//     </div>
+//   )
+// }
+
+async function getData() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+  return res.json();
+}
+
+
+export default async function Home() {
+  const res = await getData();
   return (
     <div>
       <h1>Home page</h1>
-      <Link href="/blog">Blog</Link>
+      <nav>
+        <ul>
+        {res.map((item: any) => (
+            <li key={item.id}>
+              {item.name}
+            </li>
+          ))}
+        </ul>
+      </nav>
     </div>
   )
 }
+
